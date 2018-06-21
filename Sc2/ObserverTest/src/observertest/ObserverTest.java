e7dbf34236b5faba60905f708c72137d97449a4d
SoumitSaha <1505047.sks@ugrad.cse.buet.ac.bd>
2018-06-21 14:19:54 +0600

    Observer -> project started

---
+++ Sc2/ObserverTest/src/observertest/ObserverTest.java
@@ -0,0 +1,16 @@
+package observertest;
+
+/**
+ *
+ * @author SOUMIT
+ */
+public class ObserverTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
